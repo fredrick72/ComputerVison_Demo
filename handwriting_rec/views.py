c8a4f81100d61719d2aa6b4c5eefad5207118b01
efrederi <eric.fredericksen@brooks.com>
2021-11-14 18:59:54 -0500

    commit on master (initial)

---
+++ handwriting_rec/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from datetime import date
+import calendar
+from calendar import HTMLCalendar
+
+def default(request, year = date.today().year, month = date.today().month):
+    year = int(year)
+    month = int(month)
+    if year < 1900 or year > 2099: year = date.today().year
+    month_name = calendar.month_name[month]
+    title = "Date - %s %s" % (month_name, year)
+    cal = HTMLCalendar().formatmonth(year, month)
+    # return HttpResponse("<h1>%s</h1><p>%s</p>" % (title, cal))
+    return render(request, 'handwriting_rec/base.html', {'title': title, 'cal': cal})
---
+++ landing_page/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import date
+import calendar
+from calendar import HTMLCalendar
+
+def default(request, year = date.today().year, month = date.today().month):
+    year = int(year)
+    month = int(month)
+    if year < 1900 or year > 2099: year = date.today().year
+    month_name = calendar.month_name[month]
+    title = "Date - %s %s" % (month_name, year)
+    cal = HTMLCalendar().formatmonth(year, month)
+    # return HttpResponse("<h1>%s</h1><p>%s</p>" % (title, cal))
+    return render(request, 'landing_page/base.html', {'title': title, 'cal': cal})
+

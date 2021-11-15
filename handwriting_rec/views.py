84ef593aa2268c0efd2d68fd1aadd8ca5f388f07
efrederi <eric.fredericksen@brooks.com>
2021-11-14 19:05:26 -0500

    change to base view render - remove calendar

--- handwriting_rec/views.py
+++ handwriting_rec/views.py
@@ -9,6 +9,6 @@ def default(request, year = date.today().year, month = date.today().month):
     if year < 1900 or year > 2099: year = date.today().year
     month_name = calendar.month_name[month]
     title = "Date - %s %s" % (month_name, year)
-    cal = HTMLCalendar().formatmonth(year, month)
+    
     # return HttpResponse("<h1>%s</h1><p>%s</p>" % (title, cal))
-    return render(request, 'handwriting_rec/base.html', {'title': title, 'cal': cal})
+    return render(request, 'handwriting_rec/base.html', {'title': title})
